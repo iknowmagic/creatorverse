d536428a52e13083d6d7f8bb3b06b31089986fb1
iknowmagic <ilan@iknowmagic.net>
2025-06-21 20:46:50 -0700

    feat: update Welcome component to use selected and random creators, improve layout and responsiveness

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,23 +3,23 @@ import { Navigation } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
-import { creators, categories } from '../data/creators'
+import { categories, randomCreators, selectedCreators } from '../data/creators'
 import { CreatorCard } from '../components/CreatorCard'
 import { CreatorHero } from '../components/CreatorHero'
 
 export default function Welcome() {
   return (
     <main className="flex flex-col bg-gray-100 dark:bg-gray-900 p-4 min-w-[360px] max-w-[960px] min-h-screen font-archivo uppercase">
-      <header className="flex items-center gap-4">
-        <h1 className="font-chivo text-8xl">creatorverse</h1>
-        <p className="max-w-48 text-gray-700 dark:text-gray-300 text-sm text-left">
+      <header className="flex lg:flex-row flex-col items-start lg:items-center gap-4">
+        <h1 className="font-chivo text-4xl md:text-8xl">creatorverse</h1>
+        <p className="lg:max-w-48 text-gray-700 dark:text-gray-300 text-sm text-left">
           A collection of content creators from around the world.
         </p>
       </header>
 
       <div className="divider divider-neutral"></div>
 
-      <div className="gap-2 grid grid-flow-col auto-cols-auto mb-8 text-2xl">
+      <div className="gap-2 grid sm:grid-flow-col auto-cols-auto mb-8 md:text-xl lg:text-2xl">
         {categories.map(category => (
           <div key={category}>{category}</div>
         ))}
@@ -44,7 +44,7 @@ export default function Welcome() {
           }}
           loop={true}
         >
-          {creators.map(creator => (
+          {selectedCreators().map(creator => (
             <SwiperSlide key={creator.id}>
               <CreatorHero creator={creator} />
             </SwiperSlide>
@@ -52,8 +52,8 @@ export default function Welcome() {
         </Swiper>
       </div>
 
-      <div className="gap-3 grid grid-cols-3">
-        {creators.map(creator => (
+      <div className="gap-3 grid md:grid-cols-2 lg:grid-cols-3">
+        {randomCreators().map(creator => (
           <CreatorCard key={creator.id} creator={creator} />
         ))}
       </div>
