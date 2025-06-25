c89b194eef96e04cbd439b8b7374855e9c3599be
iknowmagic <ilan@iknowmagic.net>
2025-06-24 20:50:56 -0700

    refactor: rename image-shiny-wrapper to shiny-container and update shine overlay styles for improved animation

--- app/layouts/basic/Header.tsx
+++ app/layouts/basic/Header.tsx
@@ -2,19 +2,25 @@ import Search from '~/assets/search.png'
 
 export function Header() {
   return (
-    <header className="flex lg:flex-row flex-col items-start lg:items-center gap-4 uppercase">
+    <header className="flex flex-col items-start uppercase">
       <h1 className="flex font-chivo text-4xl md:text-8xl">
         creatorverse
-        <div className="w-32 image-shiny-wrapper">
+        <div className="w-32 shiny-container">
           <img
             src={Search}
             alt="Search"
-            className="inline-block object-center object-cover"
+            className="inline-block object-center object-cover shiny-image"
+            style={{ clipPath: 'inset(5px)' }}
+          />
+          <img
+            src={Search}
+            alt="Search"
+            className="inline-block object-center object-cover shine-overlay"
             style={{ clipPath: 'inset(5px)' }}
           />
         </div>
       </h1>
-      <p className="lg:max-w-48 text-gray-700 dark:text-gray-300 text-sm text-left">
+      <p className="mt-[-40px] pl-4 w-full text- text-gray-700 text-md dark:text-gray-300 text-left tracking-[3.5px]">
         A collection of content creators from around the world.
       </p>
     </header>
