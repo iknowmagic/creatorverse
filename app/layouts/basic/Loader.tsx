9a15596f645d12e66f4f059f432cb70fcedb3e9c
iknowmagic <ilan@iknowmagic.net>
2025-06-25 17:10:54 -0700

    Add Error component to handle unexpected errors with a visual representation

--- app/layouts/basic/Loader.tsx
+++
@@ -1,8 +0,0 @@
-function Loader() {
-  return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <div className="border-4 border-gray-200 border-t-transparent border-solid rounded-full w-16 h-16 animate-spin"></div>
-    </div>
-  )
-}
-export default Loader
--- app/layouts/basic/index.tsx
+++ app/layouts/basic/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
+import { Toaster, toast } from 'sonner'
 import 'swiper/css'
 import 'swiper/css/autoplay'
 import 'swiper/css/navigation'
 import { filterByCategory, getCategories, getCreators, type Creator } from '~/lib/client'
 import { Card } from './Card'
 import { CategorySelector } from './CategorySelector'
+import { Error } from './Error'
 import { Header } from './Header'
 import { InfiniteScrolling } from './InfiniteScrolling'
 import { CardsWait, CategoriesWait } from './Wait'
@@ -12,6 +14,7 @@ import { CardsWait, CategoriesWait } from './Wait'
 export default function Welcome() {
   const [creators, setCreators] = useState([] as Creator[])
   const [categories, setCategories] = useState<string[]>([])
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     getCreators()
@@ -19,12 +22,18 @@ export default function Welcome() {
         setCreators(data)
       })
       .catch(error => {
-        console.error('Error fetching creators:', error)
+        toast.error('Error fetching creators', error)
+        setIsError(true)
       })
   }, [])
 
   useEffect(() => {
-    getCategories().then(setCategories).catch(console.error)
+    getCategories()
+      .then(setCategories)
+      .catch(() => {
+        toast.error('Error fetching categories')
+        setIsError(true)
+      })
   }, [])
 
   return (
@@ -33,46 +42,55 @@ export default function Welcome() {
 
       <div className="divider sm:divider-neutral"></div>
 
-      {categories.length == 0 && <CategoriesWait />}
+      {!isError &&
+        (categories.length == 0 ? (
+          <CategoriesWait />
+        ) : (
+          <CategorySelector
+            categories={categories}
+            onCategoryChange={(category: string | null) => {
+              if (category) {
+                filterByCategory(category)
+                  .then((data: Creator[]) => {
+                    setCreators(data)
+                  })
+                  .catch((error: unknown) => {
+                    toast.error('Error filtering creators by category')
+                    setIsError(true)
+                  })
+              } else {
+                // Reset the filter if no category is selected
+                getCreators()
+                  .then((data: Creator[]) => {
+                    setCreators(data)
+                  })
+                  .catch((error: unknown) => {
+                    toast.error('Error fetching creators')
+                    setIsError(true)
+                  })
+              }
+            }}
+          />
+        ))}
 
-      {categories.length > 0 && (
-        <CategorySelector
-          categories={categories}
-          onCategoryChange={category => {
-            if (category) {
-              filterByCategory(category)
-                .then(data => {
-                  setCreators(data)
-                })
-                .catch(error => {
-                  console.error('Error filtering creators by category:', error)
-                })
-            } else {
-              // Reset the filter if no category is selected
-              getCreators()
-                .then(data => {
-                  setCreators(data)
-                })
-                .catch(error => {
-                  console.error('Error fetching creators:', error)
-                })
-            }
-          }}
-        />
-      )}
+      {isError && <Error />}
 
-      {creators.length === 0 && <CardsWait count={12} />}
-      {creators.length > 0 && (
-        <InfiniteScrolling
-          allItems={creators}
-          renderItem={creator => <Card creator={creator} />}
-          itemWidth={296}
-          gap={12}
-          className="mt-8"
-          initialCount={12}
-          loadMoreCount={18}
-        />
-      )}
+      {!isError &&
+        (creators.length === 0 ? (
+          <CardsWait count={12} />
+        ) : (
+          <InfiniteScrolling
+            allItems={creators}
+            renderItem={(creator: Creator) => <Card creator={creator} />}
+            itemWidth={296}
+            gap={12}
+            className="mt-8"
+            initialCount={12}
+            loadMoreCount={18}
+          />
+        ))}
+
+      <Toaster position="top-right" richColors />
     </main>
   )
 }
