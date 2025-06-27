66fba4c255dfbfc66e5c73334c9a805e5a8123f7
iknowmagic <ilan@iknowmagic.net>
2025-06-26 17:03:27 -0700

    feat: add header to AdminDashboard for improved visibility and branding

--- app/pages/admin/AdminDashboard.tsx
+++ app/pages/admin/AdminDashboard.tsx
@@ -4,6 +4,9 @@ import { AdminTable } from './AdminTable'
 export function AdminDashboard() {
   return (
     <div className="flex flex-col bg-gray-100 dark:bg-gray-900 p-8 min-w-[360px] min-h-screen font-archivo">
+      <div className="mb-4 font-bold text-gray-900 dark:text-white text-4xl">
+        creatorverse :: admin dashboard
+      </div>
       <AdminTable />
     </div>
   )
